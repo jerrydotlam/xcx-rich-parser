2ce7abc248ab7bbd1883d12661afca4e767d7899
JerryLin <jerrydotlam@gmail.com>
2018-03-23 16:32:41 +0800

    testcase

---
+++ test/parser.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+
+var parser = require('..');
+var output = require('./output.json');
+
+describe('parse(strHtml)', function () {
+  it('should parse html string to array[xnode]', function () {
+    var data = fs.readFileSync(path.join(__dirname, './input.html'), 'utf8');
+    return parser
+      .parse(data)
+      .then(function (result) {
+        console.log('==================');
+        console.log(JSON.stringify(result));
+        console.log('==================');
+        // console.log(JSON.stringify(output));
+        // console.log('==================');
+        assert(JSON.stringify(result).trim() === JSON.stringify(output).trim());
+      })
+  });
+});
