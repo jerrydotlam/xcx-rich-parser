1fac9181babc83110e6793a65cfc19b5bb4bd043
JerryLin <jerrydotlam@gmail.com>
2018-03-30 11:20:56 +0800

    添加深度测试

--- test/parser.js
+++ test/parser.js
@@ -14,4 +14,32 @@ describe('parse(strHtml)', function () {
         assert(JSON.stringify(result).trim() === JSON.stringify(output).trim());
       })
   });
+  it('should parse a 1000 deptch dom tree', function () {
+    var data = fs.readFileSync(path.join(__dirname, './1000-div.html'), 'utf8');
+    return parser
+      .parse(data)
+      .then(function (result) {
+        let i = 0;
+        do {
+          assert(result.length === 1)
+          result = result[0].children;
+          i += 1;
+        } while (result && result.length > 0);
+        assert(i === 1001);
+      });
+  });
+  it('should parse a 10000 depth dom tree', function () {
+    var data = fs.readFileSync(path.join(__dirname, './10000-div.html'), 'utf8');
+    return parser
+      .parse(data)
+      .then(function (result) {
+        let i = 0;
+        do {
+          assert(result.length === 1)
+          result = result[0].children;
+          i += 1;
+        } while (result && result.length > 0);
+        assert(i === 10001);
+      });
+  });
 });
